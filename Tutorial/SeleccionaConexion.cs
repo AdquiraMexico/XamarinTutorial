5271bb7905ac7fefb536fb6c22a18e7f7b9425f1
Ulisses <ulissesmercado2012@icloud.com>
2020-06-15 08:30:52 -0500

    Actualización Paquetes

---
+++ Tutorial/SeleccionaConexion.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Tutorial
+{
+    [Activity(Label = "SeleccionaConexion")]
+    public class SeleccionaConexion : Activity
+    {
+        Button wifi;
+        Button bluetooth;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_selecciona_conexion);
+
+            wifi = FindViewById<Button>(Resource.Id.wifi);
+            bluetooth = FindViewById<Button>(Resource.Id.bluetooth);
+            wifi.Click += delegate {
+                //A Wifi
+                var intent = new Intent(this, typeof(WiFi));
+                StartActivity(intent);
+            };
+
+            bluetooth.Click += delegate {
+                //A Bluetooth
+                var intent = new Intent(this, typeof(Bluetooth));
+                StartActivity(intent);
+            };
+        }
+
+    }
+}
